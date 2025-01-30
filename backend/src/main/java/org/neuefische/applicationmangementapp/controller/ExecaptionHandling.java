27b5cc51105d58342718c00bf9115dd34c37ee9a
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2025-01-30 01:48:07 +0100

    added Testcase for NosuchId errors in JobOfferControllerTest und ApplicationControllerTest

--- backend/src/main/java/org/neuefische/applicationmangementapp/controller/ExecaptionHandling.java
+++ backend/src/main/java/org/neuefische/applicationmangementapp/controller/ExecaptionHandling.java
@@ -2,7 +2,9 @@ package org.neuefische.applicationmangementapp.controller;
 
 import org.neuefische.applicationmangementapp.execaptions.NoSuchId;
 import org.neuefische.applicationmangementapp.model.ErrorMessage;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
@@ -11,6 +13,7 @@ import java.time.Instant;
 
 public class ExecaptionHandling {
     @ExceptionHandler(NoSuchId.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorMessage handelNoSuchId(Exception exception){
         return new ErrorMessage(exception.getMessage(), Instant.now());
     }
