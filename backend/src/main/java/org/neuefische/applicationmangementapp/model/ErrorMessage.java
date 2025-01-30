246f6fdfdd896ee9b43a6ffd01d3f87adaf73ccb
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2025-01-30 10:21:01 +0100

    removed unused import

--- backend/src/main/java/org/neuefische/applicationmangementapp/model/ErrorMessage.java
+++ backend/src/main/java/org/neuefische/applicationmangementapp/model/ErrorMessage.java
@@ -1,6 +1,6 @@
 package org.neuefische.applicationmangementapp.model;
 
-import java.io.Serializable;
+
 import java.time.Instant;
 
 public record ErrorMessage(String message, Instant timestamp)  {
