073dfcb87b67c9a26fff3cb3913d0c51343a2cc3
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2025-02-05 14:35:34 +0100

    fixed sonar qube file  issue by removing tempfile

--- backend/src/main/java/org/neuefische/applicationmangementapp/service/CloudinaryService.java
+++ backend/src/main/java/org/neuefische/applicationmangementapp/service/CloudinaryService.java
@@ -6,7 +6,7 @@ import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,10 +24,9 @@ public class CloudinaryService {
 
             options.put("resource_type", "raw");
             options.put("format", "pdf");
-            File fileToUpload = File.createTempFile(file.getOriginalFilename(),"");
-            file.transferTo(fileToUpload);
+
             //NOSONAR
-            Map uploadFile=cloudinary.uploader().upload(fileToUpload,options);
+            Map uploadFile=cloudinary.uploader().upload(file.getBytes(),options);
             String publicId = (String) uploadFile.get("public_id");
              return cloudinary.url().secure(true).generate(publicId);
         } catch (Exception e) {
