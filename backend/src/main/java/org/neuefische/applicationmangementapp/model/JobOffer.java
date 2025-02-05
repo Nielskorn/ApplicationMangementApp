c53811b26a7d79db650cc9ccabd7f2514c080828
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2025-02-05 14:40:54 +0100

    removed not used import

--- backend/src/main/java/org/neuefische/applicationmangementapp/model/JobOffer.java
+++ backend/src/main/java/org/neuefische/applicationmangementapp/model/JobOffer.java
@@ -1,6 +1,6 @@
 package org.neuefische.applicationmangementapp.model;
 
-import com.mongodb.lang.Nullable;
+
 import org.springframework.data.annotation.Id;
 
 public record JobOffer(@Id String id,String Url_companyLogo,String companyName,String location,String jobTitle, String jobDescription,String LinkJobAd) {
