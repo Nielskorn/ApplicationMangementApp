b9ea4c399b21fb7e099f9b601d298b9786334af7
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2025-02-06 17:09:56 +0100

    added space by TestVariables that autoformater does not do

--- backend/src/test/java/org/neuefische/applicationmangementapp/controller/ApplicationControllerTest.java
+++ backend/src/test/java/org/neuefische/applicationmangementapp/controller/ApplicationControllerTest.java
@@ -26,67 +26,60 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 class ApplicationControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private ApplicationRepo applicationRepo;
+
     @Autowired
     ObjectMapper objectMapper;
 
     @Test
     @DirtiesContext
     void getAllApplications() throws Exception {
-        applicationRepo.saveAll(List.of(
-                new Application("test", "tester", "desc", "resume", appliStatus.OPEN, null, LocalDate.now()),
-                new Application("test2", "tester2", "desc", "resume", appliStatus.OPEN, null, LocalDate.now())
+        applicationRepo.saveAll(List.of(new Application("test", "tester", "desc", "resume", appliStatus.OPEN, null, LocalDate.now()), new Application("test2", "tester2", "desc", "resume", appliStatus.OPEN, null, LocalDate.now())
 
         ));
-        mockMvc.perform(get("/api/application")).andExpect(status().isOk()).andExpect(content().json(
-                        """
-                                [
-                                {"id": "test",
-                                "jobOfferID": "tester",
-                                "resume": "desc",
-                                "coverLetter": "resume",
-                                "appliStatus": "OPEN"
-                                },
-                                {"id": "test2",
-                                "jobOfferID": "tester2",
-                                "resume": "desc",
-                                "coverLetter": "resume",
-                                "appliStatus": "OPEN"}
-                                ]
-                                
-                                """
-                )
-        );
+        mockMvc.perform(get("/api/application")).andExpect(status().isOk()).andExpect(content().json("""
+                [
+                {"id": "test",
+                "jobOfferID": "tester",
+                "resume": "desc",
+                "coverLetter": "resume",
+                "appliStatus": "OPEN"
+                },
+                {"id": "test2",
+                "jobOfferID": "tester2",
+                "resume": "desc",
+                "coverLetter": "resume",
+                "appliStatus": "OPEN"}
+                ]
+                
+                """));
     }
 
     @Test
     @DirtiesContext
     void getApplicationById() throws Exception {
-        applicationRepo.saveAll(List.of(new Application("test", "tester", "resume", "cl", appliStatus.OPEN, null, LocalDate.of(2025, 2, 20)),
-                new Application("test2", "tester2", "desc", "resume", appliStatus.OPEN, null, LocalDate.of(2025, 5, 6))));
-        mockMvc.perform(get("/api/application/" + "test")).andExpect(status().isOk()).andExpect(content().json(
-                """
-                         {"id": "test",
-                         "jobOfferID": "tester",
-                         "resume": "resume",
-                         "coverLetter": "cl",
-                         "appliStatus": "OPEN",
-                         "reminderTime": null,
-                        "dateOfCreation": "2025-02-20"       }
-                        """
-        ));
+        applicationRepo.saveAll(List.of(new Application("test", "tester", "resume", "cl", appliStatus.OPEN, null, LocalDate.of(2025, 2, 20)), new Application("test2", "tester2", "desc", "resume", appliStatus.OPEN, null, LocalDate.of(2025, 5, 6))));
+        mockMvc.perform(get("/api/application/" + "test")).andExpect(status().isOk()).andExpect(content().json("""
+                 {"id": "test",
+                 "jobOfferID": "tester",
+                 "resume": "resume",
+                 "coverLetter": "cl",
+                 "appliStatus": "OPEN",
+                 "reminderTime": null,
+                "dateOfCreation": "2025-02-20"       }
+                """));
     }
 
     @Test
     void getApplicationByIDExpecedError() throws Exception {
-        mockMvc.perform(get("/api/application/" + "test2")).andExpect(status().isNotFound()).andExpect(content().json(
-                """
-                        {"message": "no such id: test2"}
-                        """
-        ));
+        mockMvc.perform(get("/api/application/" + "test2")).andExpect(status().isNotFound()).andExpect(content().json("""
+                {"message": "no such id: test2"}
+                """));
     }
 
     @Test
@@ -100,18 +93,16 @@ class ApplicationControllerTest {
     @Test
     @DirtiesContext
     void updateApplication() throws Exception {
-        applicationRepo.save(new Application("test1", "tester2", "resune", "cob", appliStatus.OPEN, null, LocalDate.of(2020, 1, 1)))
-        ;
-        mockMvc.perform(put("/api/application/" + "test1").contentType(MediaType.APPLICATION_JSON)
-                .content("""
-                        {
-                         "jobOfferID": "tester2",
-                         "resume": "resume",
-                         "coverLetter": "coverletter",
-                         "appliStatus": "IN_PROGRESS",
-                         "reminderTime": null
-                         }
-                        """)).andExpect(status().isOk()).andExpect(content().json("""
+        applicationRepo.save(new Application("test1", "tester2", "resune", "cob", appliStatus.OPEN, null, LocalDate.of(2020, 1, 1)));
+        mockMvc.perform(put("/api/application/" + "test1").contentType(MediaType.APPLICATION_JSON).content("""
+                {
+                 "jobOfferID": "tester2",
+                 "resume": "resume",
+                 "coverLetter": "coverletter",
+                 "appliStatus": "IN_PROGRESS",
+                 "reminderTime": null
+                 }
+                """)).andExpect(status().isOk()).andExpect(content().json("""
                 {"id": "test1",
                 "jobOfferID": "tester2",
                 "resume": "resume",
@@ -134,8 +125,7 @@ class ApplicationControllerTest {
                     "reminderTime": null
                     }
                 """;
-        mockMvc.perform(put("/api/joboffer/" + "test").contentType(MediaType.APPLICATION_JSON).
-                content(updatedApplication)).andExpect(status().isNotFound()).andExpect(content().json("""
+        mockMvc.perform(put("/api/joboffer/" + "test").contentType(MediaType.APPLICATION_JSON).content(updatedApplication)).andExpect(status().isNotFound()).andExpect(content().json("""
                 {"message": "no such id: test"}
                 """));
     }
@@ -143,26 +133,17 @@ class ApplicationControllerTest {
     @DirtiesContext
     @Test
     void expectSuccessfulPost() throws Exception {
-        String actual = mockMvc.perform(
-                        post("/api/application")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                                        
-                                            {"jobOfferID":"1a","resume": "Omar", "coverLetter": "omar","reminderTime": null}
-                                        """)
-                )
-                .andExpect(status().isOk())
-                .andExpect(content().json("""
-                             {
-                           "jobOfferID":"1a",
-                           "resume": "Omar",
-                           "coverLetter": "omar"
-                           //"reminderTime":null,
-                        }
-                        """))
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
+        String actual = mockMvc.perform(post("/api/application").contentType(MediaType.APPLICATION_JSON).content("""
+                
+                    {"jobOfferID":"1a","resume": "Omar", "coverLetter": "omar","reminderTime": null}
+                """)).andExpect(status().isOk()).andExpect(content().json("""
+                     {
+                   "jobOfferID":"1a",
+                   "resume": "Omar",
+                   "coverLetter": "omar"
+                   //"reminderTime":null,
+                }
+                """)).andReturn().getResponse().getContentAsString();
 
         Application actualApplication = objectMapper.readValue(actual, Application.class);
         assertThat(actualApplication.id()).isNotBlank();
