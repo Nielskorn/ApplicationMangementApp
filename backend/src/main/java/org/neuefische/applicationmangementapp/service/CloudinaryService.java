c95b2cefe9d7985bf88ac5901909fadcb0dfcadb
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2025-02-11 12:26:11 +0100

    Changed format and fixed pdf delivering problem

--- backend/src/main/java/org/neuefische/applicationmangementapp/service/CloudinaryService.java
+++ backend/src/main/java/org/neuefische/applicationmangementapp/service/CloudinaryService.java
@@ -6,7 +6,6 @@ import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,27 +16,24 @@ public class CloudinaryService {
     private Cloudinary cloudinary;
 
 
-    public String uploadFile(MultipartFile file) throws IOException {
-
+    public String uploadPdfFile(MultipartFile file) throws IOException {
         Map<String, String> options = new HashMap<>();
         options.put("folder", "ApplicationMangementApp");
-
         options.put("resource_type", "raw");
         options.put("format", "pdf");
-
+        
         //NOSONAR
         Map uploadFile = cloudinary.uploader().upload(file.getBytes(), options);
+
         String publicId = (String) uploadFile.get("public_id");
-        return cloudinary.url().secure(true).generate(publicId);
+        String cloudinaryResumeUrl = cloudinary.url().secure(true).resourceType("raw").generate(publicId);
+        return cloudinaryResumeUrl.replace("image", "raw");
     }
 
-
     public void deleteFile(String url) throws IOException {
-
-
         String removeUrl = url.replaceFirst("^.*?/upload/", "");
         String removeVersion = removeUrl.replaceFirst("^v\\d+/", "");
         cloudinary.uploader().destroy(removeVersion, ObjectUtils.asMap("resource_type", "raw"));
-
     }
+
 }
